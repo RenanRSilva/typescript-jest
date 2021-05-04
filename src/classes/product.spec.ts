02c8284184065b9c48bc9e828e969409d3b42644
Renan Silva <renanrambuls@gmail.com>
2021-05-04 09:58:50 -0300

    correção product e teste discount

--- src/classes/product.spec.ts
+++ src/classes/product.spec.ts
@@ -9,7 +9,7 @@ return new Product(name, price);
 describe('Product', () => {
   afterEach(() => jest.clearAllMocks());
 
-  it('should return undefined', () => {
+  it('should have properties name and price', () => {
     const sut = createSut('Camiseta', 49.9);
     expect(sut).toHaveProperty('name', 'Camiseta');
     expect(sut.price).toBeCloseTo(49.9);
