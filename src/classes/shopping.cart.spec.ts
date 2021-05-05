d4a855b883766e757ff750c7ccd8f2f477767f3b
Renan Silva <renanrambuls@gmail.com>
2021-05-05 07:28:59 -0300

    testando shopping cart com mocks

--- src/classes/shopping.cart.spec.ts
+++ src/classes/shopping.cart.spec.ts
@@ -19,6 +19,16 @@ const createCartItem = (name: string, price: number) => {
   }
 
   return new CartItemMock(name, price)
+};
+
+
+const createSutWithProducts = () => {
+  const { sut, discountMock } = createSut();
+  const cartItem1 =  createCartItem('Camiseta', 40)
+  const cartItem2 =  createCartItem('Camiseta', 1)
+  sut.addItem(cartItem1);
+  sut.addItem(cartItem2)
+  return { sut, discountMock };
 }
 
 describe('ShoppingCart', () =>{
@@ -28,11 +38,33 @@ describe('ShoppingCart', () =>{
   });
 
   it('should have 2 cart items', () => {
-    const { sut } = createSut();
-    const cartItem1 =  createCartItem('Camiseta', 40)
-    const cartItem2 =  createCartItem('Camiseta', 1)
-    sut.addItem(cartItem1);
-    sut.addItem(cartItem2)
+    const { sut } = createSutWithProducts();
     expect(sut.items.length).toBe(2);
+  });
+
+
+  it('should test total and totalWithDiscount', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.total()).toBe(41);
+    expect(sut.totalWithDicount()).toBe (41);
+  });
+
+
+  it('should add products and clear cart', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.clear()
+    expect(sut.items.length).toBe (0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+
+  it('should remove products', () => {
+    const { sut } = createSutWithProducts();
+    expect(sut.items.length).toBe(2);
+    sut.removeItem(1);
+    expect(sut.items.length).toBe(1);
+    sut.removeItem(0);
+    expect(sut.isEmpty()).toBe(true);
   });
 });
